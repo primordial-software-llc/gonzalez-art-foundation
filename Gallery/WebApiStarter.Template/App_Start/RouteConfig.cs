bdb8ee0cd3cb535dbcc448a5eda5cffe835587e0
Timothy Gonzalez <timg456789@yahoo.com>
2017-08-20 00:27:53 -0400

    new webapi project.

---
+++ Gallery/WebApiStarter.Template/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace WebApiStarter.Template
+{
+    /// <summary>
+    /// Represents route configuration.
+    /// </summary>
+    public class RouteConfig
+    {
+        /// <summary>
+        /// Configures Web API routes.
+        /// </summary>
+        /// <param name="configuration"></param>
+        public static void Configure(HttpConfiguration configuration)
+        {
+            configuration.MapHttpAttributeRoutes();
+        }
+    }
+}
