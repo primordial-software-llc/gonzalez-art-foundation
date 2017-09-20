76aa7422cfb6fdd730a59d9430412d146e21195a
Timothy Gonzalez <timg456789@yahoo.com>
2017-09-20 04:41:31 -0400

    Epiphany while waiting for s3. This is good for a console app. AWS makes things way too complex. This is such a simpler app than anything AWS was offering.

--- SlideshowCreator/SlideshowCreator/Scripts/DynamoDbToS3BackupScript.cs
+++ SlideshowCreator/SlideshowCreator/Scripts/DynamoDbToS3BackupScript.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.S3;
@@ -21,7 +22,6 @@ namespace SlideshowCreator.Scripts
         [Test]
         public void A_Write_Backup_Keys_To_File()
         {
-            
             var scanRequest = new ScanRequest(ImageClassificationAccess.IMAGE_CLASSIFICATION_V2);
 
             var allItems = new List<ClassificationModel>();
@@ -45,45 +45,31 @@ namespace SlideshowCreator.Scripts
             var expectedItems = 292105;
             Assert.AreEqual(expectedItems, allItems.Count);
 
-            var jsonItems = (from i in allItems select JsonConvert.SerializeObject(i)).ToList();
-            File.WriteAllLines("C:\\Users\\peon\\Desktop\\projects\\SlideshowCreator\\DynamoDbToS3Backup.json", jsonItems);
-        }
-
-        [Test]
-        public void B_Check_Counts_From_Read()
-        {
-            var rawItems = File.ReadAllLines("C:\\Users\\peon\\Desktop\\projects\\SlideshowCreator\\DynamoDbToS3Backup.json");
-            var expectedItems = 292105;
-            Assert.AreEqual(expectedItems, rawItems.Count());
-
-            var allItems = (from i in rawItems select JsonConvert.DeserializeObject<ClassificationModel>(i)).ToList();
-            Assert.AreEqual("http://www.the-athenaeum.org", allItems.First().Source);
-            Assert.AreEqual(33, allItems.First().PageId);
-        }
-
-        [Test]
-        public void C_Sync_To_S3()
-        {
-            var bucket = "tgonzalez-dynamodb-imageclassification-backup/" + DateTime.UtcNow.ToString("yyyy-MM-dd");
-            Regex urlRegex = new Regex("[^a-zA-Z0-9 -]");
-            var rawItems = File.ReadAllLines("C:\\Users\\peon\\Desktop\\projects\\SlideshowCreator\\DynamoDbToS3Backup.json").ToList();
-
-            for (int i = rawItems.Count - 1; i > -1; i--)
+            using (var memoryStream = new MemoryStream())
             {
-                var parsedItem = JsonConvert.DeserializeObject<ClassificationModel>(rawItems[i]);
-                var key = urlRegex.Replace(parsedItem.Source, string.Empty) + "-" + parsedItem.PageId;
+                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    var demoFile = archive.CreateEntry("dynamodb-backup.json", CompressionLevel.Optimal);
 
+                    using (var entryStream = demoFile.Open())
+                    using (var streamWriter = new StreamWriter(entryStream))
+                    {
+                        streamWriter.Write(JsonConvert.SerializeObject(allItems));
+                    }
+                }
+
+                memoryStream.Position = 0;
+                var bucket = "tgonzalez-dynamodb-imageclassification-backup";
                 PutObjectRequest request = new PutObjectRequest
                 {
                     BucketName = bucket,
-                    Key = key,
-                    ContentBody = rawItems[i]
+                    Key = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ") + ".zip",
+                    InputStream = memoryStream
                 };
-                s3Client.PutObject(request); // S3 has no bulk operation. Could go in paralle, but it woudl require a more complex thread-safe queue. Still getting core infrastructure and processes up and running. Can't start adding fancy SQS thread safe queues yet. Potentially in the future.
-
-                rawItems.RemoveAt(i);
-                File.WriteAllLines("C:\\Users\\peon\\Desktop\\projects\\SlideshowCreator\\DynamoDbToS3Backup.json", rawItems); // SSD is  must for this project.
+                s3Client.PutObject(request);
             }
+
         }
+
     }
 }
