02b3c0c59c98660ab0f400a5a8c2ea96b607d514
Timothy Gonzalez <timg456789@yahoo.com>
2018-02-08 23:46:41 -0500

    Add hard-coded list for speech recognition in alexa. Rebuild image artist table fixing whitespace.

---
+++ SlideshowCreator/SlideshowCreator/Scripts/BuildListOfArtistsJsonForAlexa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Amazon.DynamoDBv2.Model;
+using AwsTools;
+using GalleryBackend.Model;
+using IndexBackend;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace SlideshowCreator.Scripts
+{
+    class BuildListOfArtistsJsonForAlexa
+    {
+        [Test]
+        public void Build_Json_For_Manual_Copy_Into_Static_Json()
+        {
+            var client = GalleryAwsCredentialsFactory.DbClient;
+            var scanRequest = new ScanRequest(new ArtistModel().GetTable());
+            ScanResponse scanResponse = null;
+            var artists = new List<ArtistModel>();
+			
+            do
+            {
+                if (scanResponse != null)
+                {
+                    scanRequest.ExclusiveStartKey = scanResponse.LastEvaluatedKey;
+                }
+                scanResponse = client.Scan(scanRequest);
+                artists.AddRange(
+                    Conversion<ArtistModel>.ConvertToPoco(scanResponse.Items));
+            } while (scanResponse.LastEvaluatedKey.Any());
+            var container = new JArray();
+            foreach (var artist in artists.OrderBy(x => x.Artist))
+            {
+                var item = new JObject();
+                var value = new JObject();
+                value.Add("value", artist.Artist);
+                item.Add("name", value);
+                container.Add(item);
+            }
+            Console.WriteLine(container.ToString());
+        }
+    }
+}
---
+++ SlideshowCreator/SlideshowCreator/Scripts/DeployArtistNameTable.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Amazon.DynamoDBv2.Model;
+using AwsTools;
+using GalleryBackend;
+using GalleryBackend.Model;
+using IndexBackend;
+using IndexBackend.Indexing;
+using NUnit.Framework;
+using SlideshowCreator.InfrastructureAsCode;
+
+namespace SlideshowCreator.Scripts
+{
+    class DeployArtistNameTable
+    {
+
+        [Test]
+        public void A_Deploy_Artist_Name_Table()
+        {
+            var client = GalleryAwsCredentialsFactory.DbClient;
+            var tableFactory = new DynamoDbTableFactory(client);
+            var request = new CreateTableRequest
+            {
+                TableName = new ArtistModel().GetTable(),
+                KeySchema = new List<KeySchemaElement>
+                {
+                    new KeySchemaElement
+                    {
+                        AttributeName = ArtistModel.ARTIST,
+                        KeyType = "HASH"
+                    }
+                },
+                AttributeDefinitions = new List<AttributeDefinition>
+                {
+                    new AttributeDefinition
+                    {
+                        AttributeName = ArtistModel.ARTIST,
+                        AttributeType = "S"
+                    }
+                },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 1,
+                    WriteCapacityUnits = 1
+                }
+            };
+            tableFactory.CreateTable(request);
+        }
+
+        [Test]
+        public void B_Fill_Artist_Table()
+        {
+            var client = GalleryAwsCredentialsFactory.DbClient;
+            var toolsClient = new DynamoDbClient<ArtistModel>(client, new ConsoleLogging());
+
+            var scanRequest = new QueryRequest(new ClassificationModel().GetTable());
+            QueryResponse scanResponse = null;
+
+            HashSet<string> artistWorks = new HashSet<string>();
+            do
+            {
+                if (scanResponse != null)
+                {
+                    scanRequest.ExclusiveStartKey = scanResponse.LastEvaluatedKey;
+                }
+                scanRequest.ProjectionExpression = $"{ArtistModel.ARTIST}";
+                scanRequest.KeyConditions = new Dictionary<string, Condition>
+                {
+                    {
+                        "source",
+                        new Condition
+                        {
+                            ComparisonOperator = "EQ",
+                            AttributeValueList = new List<AttributeValue>
+                            {
+                                new AttributeValue {S = new TheAthenaeumIndexer().Source}
+                            }
+                        }
+                    }
+                };
+                scanResponse = client.Query(scanRequest);
+                var images = Conversion<ClassificationModel>.ConvertToPoco(scanResponse.Items)
+                    .ToList();
+                foreach (var image in images)
+                {
+                    artistWorks.Add(image.Artist);
+                }
+                
+            } while (scanResponse.LastEvaluatedKey.Any());
+
+            var toInsert = artistWorks
+                .Where(x => !string.IsNullOrWhiteSpace(x) && x.Trim().Length > 0)
+                .Select(x => new ArtistModel {Artist = x.Trim()})
+                .OrderBy(x => x.Artist)
+                .ToList();
+
+            var batches = Batcher.Batch(25, toInsert);
+            foreach (var batch in batches)
+            {
+                var batchCopy = batch.ToList();
+                while (batchCopy.Any())
+                {
+                    batchCopy = toolsClient.Insert(batchCopy).Result;
+                }
+            }
+        }
+        
+    }
+}
