4f4f15111c675ec1876bfdd2675bc38186491b68
Timothy Gonzalez <timg456789@yahoo.com>
2018-02-15 20:56:34 -0500

    remove unused.

--- SlideshowCreator/SlideshowCreator/Scripts/CleanArtistImageWorkNamesForAlexa.cs
+++ SlideshowCreator/SlideshowCreator/Scripts/CleanArtistImageWorkNamesForAlexa.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Amazon.DynamoDBv2.Model;
 using AwsTools;
 using GalleryBackend;
 using GalleryBackend.Model;
-using HtmlAgilityPack;
 using IndexBackend;
 using IndexBackend.Indexing;
 using Newtonsoft.Json;
