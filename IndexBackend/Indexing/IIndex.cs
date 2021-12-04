42f6692f5e7b7ab68d1c9b509a1545d03ef483a3
Timothy Gonzalez <timg456789@yahoo.com>
2021-12-03 23:59:39 -0500

    Allow existing record to be passed for re-indexing.

--- IndexBackend/Indexing/IIndex.cs
+++ IndexBackend/Indexing/IIndex.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading.Tasks;
+using ArtApi.Model;
+
 namespace IndexBackend.Indexing
 {
     public interface IIndex
     {
         string ImagePath { get; }
-        Task<IndexResult> Index(string id);
+        Task<IndexResult> Index(string id, ClassificationModel existing);
     }
 }
